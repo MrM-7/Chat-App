3e6759b90babb727403f115cd77083873a99cc0c
Mehul Raj <rmmehul7@gmail.com>
2024-06-17 12:00:30 +0530

    chore: Update chat bubble and background colors in Message and Conversation components

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -8,7 +8,7 @@ const Message = ({ message }) => {
 	const fromMe = message.senderId === authUser._id;
 	const chatClassName = fromMe ? "chat-end" : "chat-start";
 	const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic;
-	const bubbleBgColor = fromMe ? "bg-blue-500" : "";
+	const bubbleBgColor = fromMe ? "bg-[#354A86]" : "";
 	const formattedTime = extractTime(message.createdAt)
 
 	return (
--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -8,8 +8,8 @@ const Conversation = ({ conversation, lastIdx, emoji }) => {
 	return (
 		<>
 			<div
-				className={`flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer
-				${isSelected ? "bg-sky-500" : ""}
+				className={`flex gap-2 items-center hover:bg-[#354A86] rounded p-2 py-1 cursor-pointer
+				${isSelected ? "bg-[#354A86]" : ""}
 			`}
 				onClick={() => setSelectedConversation(conversation)}
 			>
