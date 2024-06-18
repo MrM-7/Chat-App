62b5a428d54d46b9542a3909bea89bde1e85c6a2
Mehul Raj <rmmehul7@gmail.com>
2024-06-18 12:43:12 +0530

    chore: Update server.js to serve frontend static files and handle all routes

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,13 @@ import cookieParser from "cookie-parser"
 import connectDB from "./db/index.js"
 import { app, server } from "./socket/socket.js"
 
+import path from "path"
+
 dotenv.config()
 
 const PORT = process.env.PORT || 5000
+
+const __dirname = path.resolve()
 
 app.use(express.json())  // to parse incoming requests with json payload
 app.use(cookieParser())
@@ -20,6 +24,12 @@ import userRoutes from "./routes/user.routes.js"
 app.use("/api/v1/auth", authRoutes)
 app.use("/api/v1/messages", messageRoutes)
 app.use("/api/v1/users", userRoutes)
+
+app.use(express.static(path.join(__dirname, 'frontend', 'dist')));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+})
 
 connectDB()
 .then(() => {
